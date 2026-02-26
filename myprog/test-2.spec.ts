a6516211737cbbe53373b40e93d3b3ae28aff850
kishoruprade <kishoruprade@gmai.com>
2026-02-26 12:16:22 +0530

    Commit all the files in git

--- myprog/test-2.spec.ts
+++ myprog/test-2.spec.ts
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://www.google.com/?zx=1771915582519&no_sw_cr=1');
   await page.getByRole('img', { name: 'Google' }).click();
+   await page.getByRole('img', { name: 'Google' }).click();
 });
