fbe94d7e2f51d6c8e7f9872cae856f3bccbe6d85
kishoruprade <kishoruprade@gmai.com>
2026-02-26 12:49:15 +0530

    Refactor test to improve page navigation and assertions

--- myprog/test-2.spec.ts
+++ myprog/test-2.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/?zx=1771915582519&no_sw_cr=1');
-  await page.getByRole('img', { name: 'Google' }).click();
-   await page.getByRole('img', { name: 'Google' }).click();
+  await page.goto('https://www.google.com/?zx=1771915582519&no_sw_cr=1', {
+    waitUntil: 'domcontentloaded',
+  });
+  await page.getByRole('button', { name: 'Not interested' }).click();
+  await expect(page).toHaveTitle(/Google/i);
+  await expect(page.locator('textarea[name="q"]')).toBeVisible();
 });
